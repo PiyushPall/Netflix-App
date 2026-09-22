c38770342bf9a701a52458400b70c0e1b28db503
Piyush Pal <piyush.pal272421@gmail.com>
2026-09-22 23:02:49 +0530

    Connect frontend with Render backend

--- src/components/Utils/authApi.js
+++ src/components/Utils/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
+const API_URL = `https://netflix-app-backend-018j.onrender.com/api/auth`;
 
 export const signupUser = async (userData) => {
   const response = await axios.post(`${API_URL}/signup`, userData);
